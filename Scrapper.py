3c4526fde628213f75b4a9ff0ee013c1c21bcacc
Mohit Galvankar <mgalvank@iu.edu>
2017-07-10 20:33:38 -0700

    Added files

---
+++ Scrapper.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import urllib
+import sys
+
+downloadpath = sys.argv[1]
+
+#specify the url
+weblink = "http://getcomics.info/?s=the+walking+dead"
+webpage = requests.get(weblink)
+# webpage.content
+
+list = []
+downloadurl = ""
+soup = BeautifulSoup(webpage.content,'html.parser')
+# print(soup.prettify().encode("utf-8"))
+# soup.find_all(class_ = "post-header-image")
+for link in soup.findAll('a', attrs={'href': re.compile("^http://getcomics.info/other-comics/")}):
+   list.append(link.get('href'))
+
+
+weblink2 = list[0].encode("UTF-8")
+webpage2 = requests.get(weblink2)
+webpage2.content
+
+link = list[0].encode("UTF-8").split("/")
+for items in link:
+    if items == "":
+       link.remove(items)
+
+filename = link[-1]
+
+soup2 = BeautifulSoup(webpage2.content,'html.parser')
+
+for link in soup2.findAll('a', attrs={'href': re.compile("^http://"),'title' : re.compile("Download Now")}):
+    downloadurl = link.get('href')
+
+#Can be done using requests but urllib is one liner
+urllib.urlretrieve(downloadurl,"C:/Users/Mohit/Desktop/%s.cbr" %filename)
+urllib.urlretrieve(downloadurl,"{0}/{1}.cbr".format(downloadpath, filename))
